9504165e12783042ca1e15bf9a3ad2863a7bd0cd
liks17 <likasavenkova372@gmail.com>
2023-03-16 20:13:48 +0300

    Добавлены хуки,  работа с корзиной, бэкенд

--- my-app/src/components/Card/index.js
+++ my-app/src/components/Card/index.js
@@ -1,23 +1,33 @@
 import styles from './Card.module.scss';
+import React from 'react';
 
-console.log(styles);
 
-function Card(props) {
-  return (
-    <div className={styles.card}>
-      <div className={styles.favorite}>
+function Card({ title, imageUrl, price, onFavorite, onPlus }) {
+    const [isAdded, setIsAdded] = React.useState(false);
+  
+    const onClickPlus = () => {
+      onPlus({ title, imageUrl, price });
+      setIsAdded(!isAdded);
+    };
+
+    return (
+        <div className={styles.card}>
+            <div className={styles.favorite} onClick={onFavorite}>
         <img src="/img/heart.svg" alt="Unliked" />
       </div>
-      <img width={133} height={112} src={props.imageUrl} alt="Sneakers" />
-      <h5>{props.title}</h5>
+      <img width={133} height={112} src={imageUrl} alt="Sneakers" />
+      <h5>{title}</h5>
       <div className="d-flex justify-between align-center">
         <div className="d-flex flex-column">
           <span>Цена:</span>
-          <b>{props.price} руб.</b>
+          <b>{price} руб.</b>
         </div>
-        <button className="button" onClick={props.onClick}>
-          <img width={11} height={11} src="/img/plus.svg" alt="Plus" />
-        </button>
+        <img
+          className={styles.plus}
+          onClick={onClickPlus}
+          src={isAdded ? '/img/galka.svg' : '/img/plus.svg'}
+          alt="Plus"
+        />
       </div>
     </div>
   );
