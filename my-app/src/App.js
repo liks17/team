77f30a026ec3295788d3f7f21a89a4c61f58d149
liks17 <likasavenkova372@gmail.com>
2023-03-18 12:43:48 +0300

    Тестирую оформление

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -29,7 +29,7 @@ function App() {
       <Header onClickCart={() => setCartOpened(true)} />
       <div className="content p-40">
         <div className="d-flex align-center justify-between mb-40">
-          <h1>Все кроссовки</h1>
+          <h1>Все кольца</h1>
           <div className="search-block d-flex">
             <img src="/img/search.svg" alt="Search" />
             <input placeholder="Поиск..." />
--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -2,10 +2,10 @@ function Header(props) {
     return (
       <header className="d-flex justify-between align-center p-40">
         <div className="d-flex align-center">
-          <img width={40} height={40} src="/img/logo.png" />
+          <img width={40} height={40} src="/img/logo2.png" />
           <div>
-            <h3 className="text-uppercase">React Sneakers</h3>
-            <p className="opacity-5">Магазин лучших кроссовок</p>
+            <h3 className="text-uppercase">RingReact</h3>
+            <p className="opacity-5">Интернет магазин колец</p>
           </div>
         </div>
         <ul className="d-flex">
